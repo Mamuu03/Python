2cd40ac118471b1c85fc300263072c2db118b9db
Mamuu03 <mamsuleiman24@gmail.com>
2024-03-03 12:42:13 -0800

    My first commit on python

---
+++ user_input.py
@@ -0,0 +1,7 @@
+#Input function that allows users to submit their name,age and location. 
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+location=input("Where are you from?:")
+
+#Outputs the users name,age and location.
+print("Hello",name,". You are",age,"years old","and live in",location)
